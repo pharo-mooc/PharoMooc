d2b3baf7702dfb47138d445dc7c12480a8fd0ca4
LucFabresse <luc.fabresse@gmail.com>
2020-03-18 10:02:55 +0100

    add Counter V2 to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ $CPMV $SRC/Slides/Week1/*.erratum $DEST
 $CPMV $SRC/Exercises/Exo-Expressions-LiteralObjects.pdf $DEST
 $CPMV $SRC/Exercises/Exo-Expressions-LiteralObjects-FR.pdf $DEST
 $CPMV $SRC/Exercises/Exo-Counter.pdf $DEST
+$CPMV $SRC/Exercises/Exo-Counter-V2.pdf $DEST
 $CPMV $SRC/Exercises/Exo-W1-TinyBlog-Model-EN.pdf $DEST
 $CPMV $SRC/Exercises/Exo-W1-TinyBlog-Model-FR.pdf $DEST
 $CPMV $SRC/Exercises/Challenge0-EN.pdf $DEST
